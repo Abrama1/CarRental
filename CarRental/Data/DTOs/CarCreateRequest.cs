82b02a727b113554a8d3f0b8d6e3f6a6a3865703
Davit Abramishvili <david.abramishvili10@gmail.com>
2025-03-26 23:48:23 +0400

    Implement CarCreateRequest DTO

---
+++ CarRental/Data/DTOs/CarCreateRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace CarRental.Data.DTOs
+{
+    public class CarCreateRequest
+    {
+        public string Make { get; set; } = null!;
+        public string Model { get; set; } = null!;
+        public int Year { get; set; }
+        public decimal DailyRate { get; set; }
+        public string Location { get; set; } = null!;
+        public string? LicensePlate { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+
+}
